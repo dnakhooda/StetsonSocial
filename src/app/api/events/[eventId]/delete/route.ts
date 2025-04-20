5f1552c8ac3502967c1bbdb46e6fa76936bc452c
Daniel <dnakhooda@gmail.com>
2025-04-20 15:41:09 -0400

    remove unused var

--- src/app/api/events/[eventId]/delete/route.ts
+++ src/app/api/events/[eventId]/delete/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { db } from "../../../../../../firebaseConfig";
 import { get, ref, remove } from "firebase/database";
 import Event from "@/types/event";
